965243cdff5cfb84b6f9be4cb7913f30d9ba9db8
Gage Cottom <gagecottom@hotmail.com>
2023-11-26 22:52:45 -0600

    aesthetic ui fixes. update build and version

--- main.go
+++ main.go
@@ -271,7 +271,7 @@ func init() {
 	}
 	downloadTrack = func(d string) {
 		if strings.TrimSpace(d) != "" {
-			showLabeledLoadingScreen("Fetching Track...")
+			showLabeledLoadingScreen("....Fetching  Track....")
 			tempFile, title, author, err := getTrack(d)
 			if err != nil {
 				fmt.Print("Download Error")
@@ -279,7 +279,7 @@ func init() {
 				showMainScreen()
 				return
 			}
-			showLabeledLoadingScreen("Converting Track...")
+			showLabeledLoadingScreen("...Converting  Track...")
 			err = getConverted(tempFile)
 			if err != nil {
 				fmt.Print("Download Error")
@@ -287,7 +287,7 @@ func init() {
 				showMainScreen()
 				return
 			}
-			showLabeledLoadingScreen("Fetching Metadata...")
+			showLabeledLoadingScreen("...Fetching Metadata...")
 			absolute_match := Meta{}
 			absolute_match_found := false
 			absolute_match_found, absolute_match, err = getMetaAMS(title, author)
@@ -298,7 +298,7 @@ func init() {
 				return
 			}
 			if absolute_match_found {
-				showLabeledLoadingScreen("Processing Metadata...")
+				showLabeledLoadingScreen("..Processing Metadata..")
 				tdata, fname, err := saveMeta(Meta{song: absolute_match.song, albumImage: absolute_match.albumImage, album: absolute_match.album, artist: absolute_match.artist}, tempFile)
 				if err != nil {
 					handleError(err)
